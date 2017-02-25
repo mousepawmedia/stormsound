741ad8e19af0ca78358b878d1a9b80f0f0c5d15d
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-02-25 13:23:50 -0800

    Re-tracked and added includes.

---
+++ stormsound-source/include/stormsound/dummy.hpp
@@ -0,0 +1,35 @@
+/** Dummy [Stormsound]
+  * Version: 0.1
+  *
+  * A temporary test class for Stormsound. This will be removed later
+  * when we have other functionality to build and test with.
+  *
+  * Author(s): Jason C. McDonald
+  */
+
+/* LICENSE
+ * Copyright (C) 2017 MousePaw Media.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "pawlib/iochannel.hpp"
+
+class Dummy
+{
+    public:
+        Dummy(){}
+        static void speak();
+        ~Dummy(){}
+};
